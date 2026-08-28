1709812389cceca8964e0cd82f589ededd7ca31e
stefy222 <stefpau220@gmail.com>
2026-08-28 16:36:32 -0400

    TOD-10: Eliminar una categoria (#8)

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getAll } from '../services/category.service';
+import { getAll, remove } from '../services/category.service';
 
 function CategoryList({ onEdit }) {
   const [categories, setCategories] = useState([]);
@@ -14,15 +14,37 @@ function CategoryList({ onEdit }) {
         setError("No se pudieron cargar las categorías.");
       }
     };
+
     getCategories();
   }, []);
+
+  const handleDelete = async (id) => {
+    const confirmDelete = window.confirm(
+      "¿Está seguro de que desea eliminar esta categoría?"
+    );
+
+    if (!confirmDelete) {
+      return;
+    }
+
+    try {
+      await remove(id);
+
+      setCategories(
+        categories.filter((category) => category.id !== id)
+      );
+
+    } catch (err) {
+      setError("No se pudo eliminar la categoría.");
+    }
+  };
 
   if (error) return <p className="error-msg">{error}</p>;
 
   return (
     <div className="category-view">
       <h2>Listado de Categorías</h2>
-      
+
       {categories.length === 0 ? (
         <p>No hay categorías registradas.</p>
       ) : (
@@ -31,17 +53,24 @@ function CategoryList({ onEdit }) {
             <tr>
               <th>ID</th>
               <th>Nombre</th>
+              <th>Acciones</th>
             </tr>
           </thead>
+
           <tbody>
             {categories.map((category) => (
               <tr key={category.id}>
                 <td>{category.id}</td>
                 <td>{category.nombre}</td>
+
                 <td>
-                <button onClick={() => onEdit(category)}>
-                  Editar
-                </button>
+                  <button onClick={() => onEdit(category)}>
+                    Editar
+                  </button>
+
+                  <button onClick={() => handleDelete(category.id)}>
+                    Eliminar
+                  </button>
                 </td>
               </tr>
             ))}
