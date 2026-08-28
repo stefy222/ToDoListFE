d832071ba63854cceaf4b2e345464bd1ea6d6469
stefy222 <stefpau220@gmail.com>
2026-08-28 16:18:57 -0400

    TOD-8: Crear una categoria (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 import CategoryList from "./components/CategoryList";
+import CategoryForm from "./components/CategoryForm";
 
 function App() {
   return (
     <div className="app-container">
+      <CategoryForm />
       <CategoryList />
     </div>
   );
