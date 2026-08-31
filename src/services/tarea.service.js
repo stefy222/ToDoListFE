f2d0b876c471d5fc6d1fa3b9f233a9628303d1ef
Rosibel Paucara <stefpau220@gmail.com>
2026-08-30 21:32:05 -0400

    TOD-14: Editar tarea

--- src/services/tarea.service.js
+++ src/services/tarea.service.js
@@ -1,28 +1,55 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getAll = async () => {
-  const response = await fetch(`${API_URL}/tareas`);
+    const response = await fetch(`${API_URL}/tareas`);
 
-  if (!response.ok) {
-    throw new Error("Error al obtener las tareas");
-  }
+    if (!response.ok) {
+        throw new Error("Error al obtener las tareas");
+    }
 
-  return await response.json();
+    return await response.json();
+};
+
+export const getOne = async (id) => {
+    const response = await fetch(`${API_URL}/tareas/${id}`);
+
+    if (!response.ok) {
+        throw new Error("Error al obtener la tarea");
+    }
+
+    return await response.json();
 };
 
 export const create = async (tarea) => {
-  const response = await fetch(`${API_URL}/tareas`, {
+    const response = await fetch(`${API_URL}/tareas`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
     },
     body: JSON.stringify(tarea),
-  });
+    });
 
-  if (!response.ok) {
-    throw new Error("Error al crear la tarea");
-  }
+    if (!response.ok) {
+        throw new Error("Error al crear la tarea");
+    }
 
-  return await response.json();
+    return await response.json();
+};
+
+export const update = async (id, tarea) => {
+    const response = await fetch(`${API_URL}/tareas/${id}`, {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(tarea),
+    });
+
+    if (!response.ok) {
+        throw new Error("Error al actualizar la tarea");
+    }
+
+    return await response.json();
 };
