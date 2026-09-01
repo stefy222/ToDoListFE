1510188f822f90f3477ab42bc72a2632f6a9cdf0
Rosibel Paucara <stefpau220@gmail.com>
2026-08-31 23:24:49 -0400

    TOD-20: redirección y limpieza de token

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,32 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchWithAuth = async (url, options = {}) => {
-const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-const headers = {
+    if (!token) {
+        localStorage.removeItem("token");
+        window.location.href = "/";
+    return;
+    }
+
+    const headers = {
     "Accept": "application/json",
     ...options.headers,
-};
+    "Authorization": `Bearer ${token}`,
+    };
 
-if (token) {
-    headers["Authorization"] = `Bearer ${token}`;
-}
-
-return fetch(url, {
+    const response = await fetch(url, {
     ...options,
     headers,
-});
-};
+    });
+
+    if (response.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/";
+    return;
+    }
+
+    return response;
+    };
 
 export { API_URL };
